5bc82f1cc6d820d1a741882559883569a71cf8c0
Tones <tonymannino@me.com>
2020-08-22 17:21:14 -0700

    New release

--- src/lib/service/youtube/youtube.service.ts
+++ src/lib/service/youtube/youtube.service.ts
@@ -12,10 +12,23 @@ export class YoutubeService {
     constructor(private config: AppConfig, private http: HttpClient) { }
 
     googleApiUrl: string = "https://www.googleapis.com/youtube/v3/search?";
-    apiKey: string = this.config.getConfig('youtube').apikey;
-    
+    apiKeys: string = this.config.getConfig('youtube').data;
+
+    // Start each client off on a random count, 
+    // so that all users don't always use the first API Key
+    requestCount: number = Math.round(Math.random() * (this.apiKeys.length - 1));
+
     searchYoutube(song: SpotifySong): Observable<YoutubeSearch> {
-        let url = this.googleApiUrl + "q=" + song.artist + ' ' + song.song + ' Official Video'+ '&maxResults=1&part=snippet&key=' + this.apiKey;
+        const apiKey = this.getApiKey();
+        let url = this.googleApiUrl + "q=" + song.artist + ' ' + song.song + ' Official Video'+ '&maxResults=1&part=snippet&key=' + apiKey;
         return this.http.get<YoutubeSearch>(encodeURI(url));
+    }
+
+    // Round robin through the keys in order
+    getApiKey() {
+        const key = this.apiKeys[this.requestCount % this.apiKeys.length];
+        this.requestCount += 1;
+        console.log(key, this.requestCount);
+        return key;
     }
 }
